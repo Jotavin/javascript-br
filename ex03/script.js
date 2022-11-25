fe6c68c05eb4662c96c04b32554796af4e1aa8ab
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-11-25 20:25:02 -0300

    ex03

---
+++ ex03/script.js
@@ -0,0 +1,9 @@
+// Condicionais - Switch
+var nota = 2.5;
+
+if (nota >= 7) {
+    console.log('Você passou com a nota ' + nota + '.')
+}
+else {
+    console.log('Você reprovou com a nota ' + nota + '.')
+}
