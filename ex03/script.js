7ce84048f95bf202df91afc38a4a78ee60b84805
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-11-28 16:37:53 -0300

    Update script.js

--- ex03/script.js
+++ ex03/script.js
@@ -1,9 +1,33 @@
 // Condicionais - Switch
-var nota = 2.5;
+// var nota = 2.5;
 
-if (nota >= 7) {
-    console.log('Você passou com a nota ' + nota + '.')
+// if (nota >= 7) {
+//     console.log('Você passou com a nota ' + nota + '.')
+// }
+// else {
+//     console.log('Você reprovou com a nota ' + nota + '.')
+// }
+
+var nota1 = 8;
+var nota2 = 9;
+
+var media = (nota1 + nota2) / 2;
+
+var conceito = ""
+
+if(media >= 8){
+
+    conceito = "Ótimo";
 }
-else {
-    console.log('Você reprovou com a nota ' + nota + '.')
+else if(media > 6.5){
+
+    conceito = "Bom";
 }
+else{
+
+    conceito = "Regular";
+}
+
+
+console.log(media)
+console.log(conceito)
