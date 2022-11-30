7ae321a0a237a7598a91574f57912e0559650c17
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-11-29 21:31:19 -0300

    Update script.js

--- ._javascript/script.js
+++ ._javascript/script.js
@@ -3,11 +3,11 @@ var nota1 = [5.4, 9.2, 7.1, 7.6, 3.2];
 var nota2 = [1.9, 5, 9.4, 8, 6];
 
 function media(n1, n2){
-    (n1 + n2) / 2;
-    return
+    return (n1 + n2) / 2;
+     
 }
 
 for (var index in aluno){
 
-    console.log(aluno[index] + " - " + nota1 + " - " + nota2 + " - " + media(nota1, nota2));
+    console.log(aluno[index] + " - " + nota1[index] + " - " + nota2[index] + " - " + media(nota1[index], nota2[index]));
 }
