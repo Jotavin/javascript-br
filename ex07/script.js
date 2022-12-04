0f161bfa9b12cc27e44e26e0a53c969b29dd12d0
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-12-04 18:24:07 -0300

    Update script.js

--- ex07/script.js
+++ ex07/script.js
@@ -1,5 +1,18 @@
 // Objetos
 
-var aluno = {nome:"Igor", notas:[7.5, 5.0]}
+var aluno = {
+            nome:"Igor", 
+            notas:[7.5, 5.0]
+            }
+
+
+aluno.matricula = 1234;
+
+var novaProp = 'Sobrenome'
+
+aluno[novaProp] = 'Oliveira'
+
+
 
 console.log(aluno['nome']);
+console.log(aluno);
